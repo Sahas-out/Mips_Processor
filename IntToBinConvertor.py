b1fd90c362c6a365c6071c6563ac7ec848daf93a
Sahas Sangal <sangal.s001@gmail.com>
2024-02-14 12:33:29 +0530

    Draft 2 Pc class

--- IntToBinConvertor.py
+++ IntToBinConvertor.py
@@ -1,2 +1,3 @@
+
 def IntToBin(integer):
     return bin(integer)[2:].rjust(32,'0')
--- Pc.py
+++ Pc.py
@@ -8,14 +8,13 @@ class PC:
         Instruction_0_25 = Instruction_0_25[2:] + "00" #left shift by 2
         Instruction_0_25 = self.counter[28:32] + Instruction_0_25 # adding 4 MSB from PC + 4
         SignImm = SignImm[2:] + "00" #left shift by 2
-        if(int(Branch,2) and int(Zero,2)): # this is the PCSrc line 
+        if(int(Branch,2) and int(Zero,2)): # implementaion of Mux with PcSrc as select line
             self.counter = IntToBin( int(self.counter) + int(SignImm,2) ) # doing PC+4 +imm*4
         else:
             self.counter = self.counter
-        if(int(Jump,2)):
-            self.counter = Instruction_0_25
+        if(int(Jump,2)): # implemenation of Mux with Jump as select line
+            self.counter = Instruction_0_25 # assinging Pc to Jump address after concatenation
         else:
             self.counter = self.counter
-    
         
 
